7a6bd3b672d8af3a6fdd922e5ecda30ed54cd942
Mason Phillips <mjphillips2000@gmail.com>
2020-10-02 20:44:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+   elsif phrase != phrase.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
   end
     
 end
